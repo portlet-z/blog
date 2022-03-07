20c1d915bc739c1638a341a34c3f0bee6e6b00c1
张新征 <programer.email@gmail.com>
2022-03-07 08:12:40 +0800

    MOV指令

--- logical_circuit/controller.py
+++ logical_circuit/controller.py
@@ -7,6 +7,49 @@ filename = os.path.join(dirname, 'micro.bin')
 
 micro = [pin.HTL for _ in range(0x10000)]
 
+
+def compile_addr2(addr, ir, psw, index):
+    global micro
+
+    op = ir & 0xf0
+    amd = (ir >> 2) & 3
+    ams = ir & 3
+
+    INST = ASM.INSTRUCTIONS[2]
+    if op not in INST:
+        micro[addr] = pin.CYC
+        return
+    am = (amd, ams)
+    if am not in INST[op]:
+        micro[addr] = pin.CYC
+        return
+    EXEC = INST[op][am]
+    if index < len(EXEC):
+        micro[addr] = EXEC[index]
+    else:
+        micro[addr] = pin.CYC
+
+
+def compile_addr1(addr, ir, psw, index):
+    pass
+
+
+def compile_addr0(addr, ir, psw, index):
+    global micro
+
+    op = ir
+
+    INST = ASM.INSTRUCTIONS[2]
+    if op not in INST:
+        micro[addr] = pin.CYC
+        return
+    EXEC = INST[op]
+    if index < len(EXEC):
+        micro[addr] = EXEC[index]
+    else:
+        micro[addr] = pin.CYC
+
+
 for addr in range(0x10000):
     ir = addr >> 8
     psw = (addr >> 4) & 0xf
@@ -14,6 +57,19 @@ for addr in range(0x10000):
 
     if cyc < len(ASM.FETCH):
         micro[addr] = ASM.FETCH[cyc]
+        continue
+
+    addr2 = ir & (1 << 7)
+    addr1 = ir & (1 << 6)
+
+    index = cyc - len(ASM.FETCH)
+
+    if addr2:
+        compile_addr2(addr, ir, psw, index)
+    elif addr1:
+        compile_addr1(addr, ir, psw, index)
+    else:
+        compile_addr0(addr, ir, psw, index)
 
 with open(filename, 'wb') as file:
     for var in micro:
